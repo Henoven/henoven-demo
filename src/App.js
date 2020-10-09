1d551bb0fa9618f61207b989ed71a6ae32702b36
Sebastián Rodríguez Maldonado <0210534@up.edu.mx>
2020-10-09 09:47:33 -0500

    httpService implementado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import axios from './axios';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+        const params = new URLSearchParams();
+        params.append("func", "User-li");
+        params.append("args", JSON.stringify({ Email: "daniel@gmail.com", Password:"123"}));
+        axios.post("", params)
+        .then((response) => {
+            let validar = JSON.stringify(response);
+            if(validar.includes("FrontError|")){
+                console.log(response.data);
+            }
+            else{
+                console.log(response);
+            }
+        })
+        .catch(console.log("Error"))
+    }, )
+
   return (
     <div className="App">
       <header className="App-header">
