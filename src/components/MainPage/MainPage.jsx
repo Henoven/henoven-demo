6ac6427b3e5bc0f1f4d2ac9f34476c1a36f976db
Sebastián Rodríguez Maldonado <0210534@up.edu.mx>
2020-11-02 17:07:53 -0600

    Al comprimir el submenu cambia el icono de henoven

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -13,22 +13,7 @@ import {
 
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
-  {/* <Menu.Item key="1" icon={<HomeOutlined style={{fontSize: "18px"}} />} className={styles.icon}>
-              Inicio
-            </Menu.Item>
-            <Menu.Item key="2" icon={<GlobalOutlined style={{fontSize: "18px"}} />} className={styles.icon}
-            onClick = {()=> handleOnNavigate("travels")}>
-              Mis Viajes
-            </Menu.Item>
-            <Menu.Item key="3" icon={<TeamOutlined style={{fontSize: "18px"}} />} className={styles.icon}>
-              Equipos
-            </Menu.Item>
-            <Menu.Item key="4" icon={<MobileOutlined style={{fontSize: "18px"}}/>} className={styles.icon}>
-              Dispositivos
-            </Menu.Item>
-            <Menu.Item key="5" icon={<SettingOutlined style={{fontSize: "18px"}}/>} className={styles.icon}>
-              Cuenta
-            </Menu.Item> */}
+
 const MainPage = ({children}) =>{
     const menu = [
         // {name: "inicio", route: "teams"},
@@ -64,7 +49,9 @@ const MainPage = ({children}) =>{
     return (
     <Layout className={styles.layout}>
         <Sider collapsible collapsed={Collapsed} onCollapse={(collapsed) => setCollapsed(collapsed)} theme="light" className={styles.sider}>
-          <Image src="http://henovenalfa.000webhostapp.com/resources/logo_henoven.png" preview={false} className={styles.logo}/>
+          {!Collapsed ? <Image src="http://henovenalfa.000webhostapp.com/resources/logo_henoven.png" preview={false} className={styles.logo}/> 
+              :  <Image src="http://henovenalfa.000webhostapp.com/resources/logo.png" preview={false} className={styles.logo}/> 
+          }
           <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
             {menu.map((menuItem, index)=> 
             <Menu.Item key={index} icon ={getIconComponent(menuItem.iconComponent)} onClick={()=> handleOnNavigate(menuItem.route)}>{capitalizeFirstLetter(menuItem.name)}</Menu.Item>)}
