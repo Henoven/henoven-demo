ddc3ab7fe6a2ae9f82afc7856e73c67ead6dc06d
Sebastián Rodríguez Maldonado <0210534@up.edu.mx>
2020-11-17 09:16:23 -0600

    Maquetado de modal para agregar equipos

--- src/components/Cards/CardTeam.jsx
+++ src/components/Cards/CardTeam.jsx
@@ -3,19 +3,21 @@ import {Row, Col, Button, Tooltip} from "antd";
 import { EditOutlined, LogoutOutlined } from '@ant-design/icons';
 import styles from "./cardTeam.module.css"
 
-const CardTeam = ({title, ...rest}) =>{
+const CardTeam = ({title, onEdit, onExit, idTeam,...rest}) =>{
     return(
-        <Row style={{backgroundColor: "white", width:"95%", height:"40px", borderRadius:10, borderColor:"black", borderWidth:1, fontSize:15, color:"#606060"}} align="middle">
+        <Row style={{backgroundColor: "white", width:"100%", height:"40px", borderRadius:10, fontSize:15, color:"#606060"}} align="middle">
             <Col flex="auto">
                 <span style={{marginLeft:25, textAlign:"center"}}>{title}</span>
             </Col>
             <Col flex="80px">
                 <Row align="middle" justify="space-around">
-                    <Tooltip title="search">
-                        <Button shape="circle" icon={<EditOutlined style={{color:"#3498db"}}/>} />
+                    <Tooltip title="Editar">
+                        <Button shape="circle" icon={<EditOutlined style={{color:"#3498db"}}
+                        onClick={()=> onEdit(idTeam)}/>} />
                     </Tooltip>
-                    <Tooltip title="search">
-                        <Button shape="circle" icon={<LogoutOutlined style={{color:"#e74d3d"}}/>} />
+                    <Tooltip title="Salir">
+                        <Button shape="circle" icon={<LogoutOutlined style={{color:"#e74d3d"}}
+                        onClick={()=> onExit(idTeam)}/>} />
                     </Tooltip>
                 </Row>
             </Col>            
--- src/views/Teams/Teams.jsx
+++ src/views/Teams/Teams.jsx
@@ -1,9 +1,35 @@
 import React, {useState, useEffect} from 'react';
-import { List, message } from 'antd';
+import { List, message, Tooltip, Button, Row, Modal, Input } from 'antd';
 import CardTeam from '../../components/Cards/CardTeam';
+import { PlusCircleOutlined} from '@ant-design/icons';
 import axios from "../../axios";
 
+const NewTeamModal = ({ onCancel, onOk, visible}) =>{
+    const [TeamName, setTeamName] = useState("");
+    return(<Modal
+        title="Crear nuevo equipo"
+        visible={visible}
+        onOk={onOk}
+        onCancel={onCancel}
+        footer={[
+            <Button onClick={onCancel}>
+              Cancelar
+            </Button>,
+            <Button type="primary" onClick={onOk}>
+              Crear
+            </Button>,
+          ]}
+      >
+        <Input placeholder="Nombre de equipo" onChange={(value)=> {setTeamName(value); console.log(TeamName);}}/>
+      </Modal>);
+}
+
 const Teams = ({user, ...rest}) =>{
+    
+    const [isLoading, setIsLoading] = useState(false);
+    const [teams, setTeams] = useState([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
     useEffect(() => {
       setIsLoading(true);
       const params = new URLSearchParams();
@@ -20,6 +46,7 @@ const Teams = ({user, ...rest}) =>{
           else{
             console.log(response.data);
               const { Teams, Invitations } = response.data;
+              console.log(Teams);
               if(teams) {
                   setTeams(Teams);
               }
@@ -29,19 +56,46 @@ const Teams = ({user, ...rest}) =>{
           console.log(error);
       })
     }, []);
-      const [isLoading, setIsLoading] = useState(false);
-      const [teams, setTeams] = useState([]);
+    const handleOnNewTeam = () =>{
+        setIsModalVisible(true);
+        console.log("new team")
+    } 
+    const handleOnEditTeam = (idTeam) =>{
+        console.log("edit team", idTeam)
+    } 
+    const handleOnExitTeam = (idTeam) =>{
+        console.log("exit team", idTeam)
+    } 
+    const handleOk = (newTeamName) => {
+        console.log(newTeamName);
+        setIsModalVisible(false);
+      };
+    
+     const handleCancel = e => {
+        setIsModalVisible(false);
+      };
+    
 return  (
 
     <>
-        <span>Equipos de: {user.FirstName}</span>
+        <NewTeamModal visible={isModalVisible} onOk={handleOk}
+        onCancel={handleCancel}/>
+        <Row align="middle" justify="end" style={{marginTop:10}}>
+            <Tooltip title="Crear nuevo equipo">
+                <Button shape="circle" icon={<PlusCircleOutlined style={{color:"#3498db", fontSize:25}}
+                onClick={handleOnNewTeam}/>} />
+            </Tooltip>
+        </Row>
         <List
         itemLayout="horizontal"
         loading = {isLoading}
         dataSource={teams}
         renderItem={item => (
             <List.Item style={{paddingBottom:1}}>
-               <CardTeam title={item.TeamName}/>
+               <CardTeam title={item.TeamName} 
+               idTeam={item.IdTeam}
+               onEdit={handleOnEditTeam}
+               onExit={handleOnExitTeam}/>
             </List.Item>
             )}
         />
