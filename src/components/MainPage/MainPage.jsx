f190c43aa4e5c795b580ffbc1252e6d91b477cd8
Sebastián Rodríguez Maldonado <0210534@up.edu.mx>
2020-12-19 19:04:08 -0600

    cerrar sesión implementado

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
-import { Layout, Menu, Image } from 'antd';
+import { Layout, Menu, Image, Button } from 'antd';
 import styles from "./mainPage.module.css";
 import { useHistory } from 'react-router-dom';
-
 import {
     GlobalOutlined,
     TeamOutlined,
     MobileOutlined,
-    SettingOutlined
+    SettingOutlined,
+    ImportOutlined,
+    UserOutlined,
   } from '@ant-design/icons';
+
+import { doLogOut } from "../../redux/actions/sesion";
+
+const {SubMenu} = Menu;
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -18,7 +23,6 @@ const MainPage = ({children}) =>{
         {name: "mis viajes", route: "travels", iconComponent:"global_outlined"},
         {name: "equipos", route: "teams", iconComponent:"team_outlined"},
         {name: "dispositivos", route: "devices", iconComponent:"mobile_outlined"},
-        {name: "cuenta", route: "settings", iconComponent:"setting_outlined"},
     ];
     const history = useHistory();
     const [Collapsed, setCollapsed] = useState(false);
@@ -26,6 +30,10 @@ const MainPage = ({children}) =>{
       console.log(link);
       history.push(`/${link}`);
     };
+    function handleLogout(){
+      doLogOut();
+      handleOnNavigate("");
+    }
     function capitalizeFirstLetter(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     }
@@ -39,6 +47,10 @@ const MainPage = ({children}) =>{
             return <MobileOutlined style={{fontSize: "18px"}} />;
         case "setting_outlined": 
             return <SettingOutlined style={{fontSize: "18px"}} />;
+        case "import-outlined":
+            return <ImportOutlined style={{fontSize: "18px", color:"red"}} />  
+        case "user-outlined":
+            return <UserOutlined style={{fontSize: "18px"}} />  
         default:
           break;
       }
@@ -46,13 +58,44 @@ const MainPage = ({children}) =>{
 
     return (
     <Layout className={styles.layout}>
-        <Sider collapsible collapsed={Collapsed} onCollapse={(collapsed) => setCollapsed(collapsed)} theme="light" className={styles.sider}>
+        <Sider 
+          collapsible 
+          collapsed={Collapsed} 
+          onCollapse={(collapsed) => setCollapsed(collapsed)} 
+          theme="light" 
+          className={styles.sider}>
           {!Collapsed ? <Image src="http://henovenalfa.000webhostapp.com/resources/logo_henoven.png" preview={false} className={styles.logo}/> 
               :  <Image src="http://henovenalfa.000webhostapp.com/resources/logo.png" preview={false} className={styles.logo}/> 
           }
           <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
             {menu.map((menuItem, index)=> 
-            <Menu.Item key={index} icon ={getIconComponent(menuItem.iconComponent)} onClick={()=> handleOnNavigate(menuItem.route)}>{capitalizeFirstLetter(menuItem.name)}</Menu.Item>)}
+            <>
+              <Menu.Item 
+                key={index} 
+                icon ={getIconComponent(menuItem.iconComponent)} 
+                onClick={()=> handleOnNavigate(menuItem.route)}>
+                  {capitalizeFirstLetter(menuItem.name)}
+              </Menu.Item>
+              <Menu.Divider />
+            </>  
+            )}
+          </Menu>
+          <Menu >
+            <SubMenu 
+              title="Ajustes"
+              icon={getIconComponent("setting_outlined")}>
+              <Menu.Item
+                onClick={()=> handleOnNavigate("settings")}
+                icon={getIconComponent("user-outlined")}>
+                Cuenta
+              </Menu.Item>
+              <Menu.Divider/>
+              <Menu.Item
+                icon={getIconComponent("import-outlined")}
+                onClick={()=> handleLogout()}>
+                Cerrar sesión
+              </Menu.Item>
+            </SubMenu>
           </Menu>
         </Sider>
         <Layout className={styles.contentLayout}>
